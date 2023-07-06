ad82e57fbfa836fe471ae4630fec0b72464abd9a
Hakmin Kim <ringo.cozy@gmail.com>
2023-07-06 11:17:05 +0900

    Length checker

---
+++ MyProject/SimilarityChecker1.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+double checkStringLength(const std::string& strA, const std::string& strB) {
+    double lenA = static_cast<double>(strA.length());
+    double lenB = static_cast<double>(strB.length());
+    double gab = std::abs(lenA - lenB);
+    double maxLen = std::max(lenA, lenB);
+
+    double score = (1 - (gab / maxLen)) * 60.0;
+    return score;
+}
--- MyUnittest/test.cpp
+++ MyUnittest/test.cpp
@@ -1,6 +1,20 @@
 #include "pch.h"
+#include "../MyProject/SimilarityChecker1.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(CheckStringLengthTest, SameLength) {
+    std::string strA = "Hello";
+    std::string strB = "World";
+    double expectedScore = 60.0;
+
+    double score = checkStringLength(strA, strB);
+    EXPECT_DOUBLE_EQ(score, expectedScore);
+}
+
+TEST(CheckStringLengthTest, DifferentLength) {
+    std::string strA = "OpenAI";
+    std::string strB = "GPT-3";
+    double expectedScore = 26.25;
+
+    double score = checkStringLength(strA, strB);
+    EXPECT_DOUBLE_EQ(score, expectedScore);
 }
