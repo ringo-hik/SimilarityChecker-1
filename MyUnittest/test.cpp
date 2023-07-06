e282c0eb34c6909c40d877ba1fd20868137ae883
Hakmin Kim <ringo.cozy@gmail.com>
2023-07-06 11:27:18 +0900

    Length checker

--- MyUnittest/test.cpp
+++ MyUnittest/test.cpp
@@ -1,20 +1,22 @@
 #include "pch.h"
 #include "../MyProject/SimilarityChecker1.cpp"
 
-TEST(CheckStringLengthTest, SameLength) {
-    std::string strA = "Hello";
-    std::string strB = "World";
+TEST(StringCheckerTest, SameLength) {
+    StringChecker checker;
+    std::string strA = "ABC";
+    std::string strB = "ABC";
     double expectedScore = 60.0;
 
-    double score = checkStringLength(strA, strB);
+    double score = checker.checkStringLength(strA, strB);
     EXPECT_DOUBLE_EQ(score, expectedScore);
 }
 
-TEST(CheckStringLengthTest, DifferentLength) {
-    std::string strA = "OpenAI";
-    std::string strB = "GPT-3";
+TEST(StringCheckerTest, DifferentLength) {
+    StringChecker checker;
+    std::string strA = "ABBA";
+    std::string strB = "ABB";
     double expectedScore = 26.25;
 
-    double score = checkStringLength(strA, strB);
+    double score = checker.checkStringLength(strA, strB);
     EXPECT_DOUBLE_EQ(score, expectedScore);
 }
